fcd6d3a784b4c92d1a6671c6b4db58996636496f
pizduk666 <98029366+pizduk666@users.noreply.github.com>
2022-03-11 13:39:28 +0300

    Add files via upload

---
+++ pygames/alien_invasion.py
@@ -0,0 +1,30 @@
+import sys
+from settings import Settings
+from ship import Ship
+from alien import Alien
+import game_functions as gf
+import pygame
+from pygame.sprite import Group
+
+def run_game():
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+    alien = Alien(ai_settings, screen)
+    while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        
+        gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+        print(len(bullets))
+        gf.update_aliens(ai_settings, ship, aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+run_game()
